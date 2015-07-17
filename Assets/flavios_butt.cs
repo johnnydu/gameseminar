e3d5a07454bbe557f9d136a3c83ab18d530a96d9
johnnydu <johnnydu@Johnnys-MacBook-Air.local>
2015-07-17 14:02:08 +0900

    need respawn 3

---
+++ Assets/flavios_butt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flavios_butt : MonoBehaviour {
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collision) {
+		if(collision.gameObject.CompareTag("portalfujii")) {
+        	player.transform.position = GameObject.Find("portalnattaon").transform.position;
+    	}
+
+    	if(collision.gameObject.CompareTag("portalnattaon")) {
+        	player.transform.position = GameObject.Find("portalfujii").transform.position;
+    	}
+    }
+}
